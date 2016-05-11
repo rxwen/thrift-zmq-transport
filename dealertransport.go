23b1bba1b3f40185a617fa3cc7ac4ad9dea02bb9
Raymond Wen <rx.wen218@gmail.com>
2016-05-11 19:11:21 +0800

    proof of concept version ok

---
+++ dealertransport.go
@@ -0,0 +1,69 @@
+package zmqtransport
+
+import (
+	"fmt"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+// TDeleaerTransport is a TTransport implementation.
+type TDealerTransport struct {
+	sock     *zmq.Socket
+	endpoint string
+}
+
+func NewTDealerTransport(endpoint string) TDealerTransport {
+	sock, _ := zmq.NewSocket(zmq.DEALER)
+
+	return TDealerTransport{
+		sock:     sock,
+		endpoint: endpoint,
+	}
+}
+
+func (t TDealerTransport) Open() error {
+	fmt.Println("Open", t.endpoint)
+	return t.sock.Connect(t.endpoint)
+}
+
+func (t TDealerTransport) IsOpen() bool {
+	fmt.Println("IsOpen")
+	return true
+}
+
+func (t TDealerTransport) Close() error {
+	fmt.Println("Close")
+	return t.sock.Close()
+}
+
+func (t TDealerTransport) Read(buf []byte) (l int, err error) {
+	fmt.Println("about to Read num of bytes ", len(buf))
+	data, err := t.sock.RecvBytes(0)
+	//fmt.Println("received num of bytes ", len(data), data)
+	l = 0
+	for i, b := range data {
+		if i < len(buf) {
+			l = i + 1
+			buf[i] = b
+		} else {
+			break
+		}
+	}
+	//fmt.Println("Read", buf, err)
+	return l, err
+}
+
+func (t TDealerTransport) Write(buf []byte) (int, error) {
+	fmt.Println("Write ", buf)
+	return t.sock.SendBytes(buf, 0)
+}
+
+func (p TDealerTransport) Flush() error {
+	fmt.Println("Flush")
+	return nil
+}
+
+func (p TDealerTransport) RemainingBytes() (num_bytes uint64) {
+	fmt.Println("RemainingBytes, hard code to 128")
+	return uint64(128)
+}
---
+++ routertransport.go
@@ -0,0 +1,164 @@
+package zmqtransport
+
+import (
+	"fmt"
+	zmq "github.com/pebbe/zmq4"
+
+	"git.apache.org/thrift.git/lib/go/thrift"
+)
+
+type WriteMessage struct {
+	id   []byte
+	data []byte
+}
+
+// TRouterServerTransport is a TServerTransport implementation.
+type TRouterServerTransport struct {
+	transports map[string]TRouterTransport
+	sock       *zmq.Socket
+	endpoint   string
+	cwrite     chan WriteMessage
+	caccept    chan []byte
+}
+
+func NewTRouterServerTransport(endpoint string) TRouterServerTransport {
+	fmt.Println("new TRouterServerTransport")
+	sock, err := zmq.NewSocket(zmq.ROUTER)
+
+	if err != nil {
+	}
+
+	cwrite := make(chan WriteMessage, 100)
+	caccept := make(chan []byte, 100)
+	go func() {
+		for {
+			msg := <-cwrite
+			sock.SendMessage(msg.id, msg.data)
+			fmt.Println("send ", msg)
+		}
+	}()
+
+	t := TRouterServerTransport{
+		transports: make(map[string]TRouterTransport),
+		sock:       sock,
+		endpoint:   endpoint,
+		caccept:    caccept,
+		cwrite:     cwrite,
+	}
+	go func() {
+		for {
+			id, err := sock.RecvBytes(0)
+			var transport TRouterTransport
+			if err == nil {
+				buf, _ := sock.RecvBytes(0)
+
+				if trans, ok := t.transports[string(id)]; !ok {
+					c := make(chan []byte, 1000)
+					transport = TRouterTransport{
+						id:     id,
+						cread:  c,
+						cwrite: t.cwrite,
+					}
+					fmt.Println("create transport channel for", transport)
+
+					t.transports[string(id)] = transport
+					//fmt.Println("signal accept ", id, caccept)
+					caccept <- id
+					fmt.Println("signal accept ok", id)
+				} else {
+					transport = trans
+				}
+				//fmt.Println("put data to transport channel ", buf, transport)
+				transport.cread <- buf
+				//fmt.Println("put data to transport channel ok", buf, transport)
+			}
+		}
+	}()
+	return t
+}
+
+func (t TRouterServerTransport) Listen() error {
+	t.sock.Bind(t.endpoint)
+	fmt.Println("TRouterServerTransport.Listen")
+	return nil
+}
+
+func (t TRouterServerTransport) Accept() (thrift.TTransport, error) {
+	id := <-t.caccept
+	fmt.Println("TRouterServerTransport.Accept ", id, t.transports[string(id)])
+	return t.transports[string(id)], nil
+}
+
+func (t TRouterServerTransport) Close() error {
+	fmt.Println("TRouterServerTransport.Close")
+	return nil
+}
+
+func (t TRouterServerTransport) Interrupt() error {
+	fmt.Println("TRouterServerTransport.Interrupt")
+	return nil
+}
+
+// TRouterTransport is a TTransport implementation.
+type TRouterTransport struct {
+	id     []byte
+	cread  chan []byte
+	cwrite chan WriteMessage
+}
+
+func (t TRouterTransport) GetMapKey() string {
+	return string(t.id)
+}
+
+//func NewTRouterTransport(c chan []byte) TRouterTransport {
+//return TRouterTransport{c: c}
+//}
+
+func (t TRouterTransport) Open() error {
+	return nil
+}
+
+func (t TRouterTransport) IsOpen() bool {
+	return true
+}
+
+func (t TRouterTransport) Close() error {
+	return nil
+}
+
+func (t TRouterTransport) Read(buf []byte) (l int, err error) {
+	fmt.Println("TRouterTransport.Read wait, len ", len(buf), buf)
+	data := <-t.cread
+	//fmt.Println("TRouterTransport.Read got, len ", len(data), data)
+	for i, b := range data {
+		buf[i] = b
+	}
+	fmt.Println("TRouterTransport.Read ", buf)
+	return len(data), nil
+	//buf = <-t.cread
+	//return len(buf), nil
+}
+
+func (t TRouterTransport) Write(buf []byte) (int, error) {
+	data := make([]byte, len(buf))
+	for i, b := range buf {
+		data[i] = b
+	}
+	msg := WriteMessage{
+		id:   t.id,
+		data: data,
+	}
+
+	t.cwrite <- msg
+	fmt.Println("write ", msg)
+	return len(buf), nil
+}
+
+func (p TRouterTransport) Flush() error {
+	return nil
+}
+
+func (p TRouterTransport) RemainingBytes() (num_bytes uint64) {
+	fmt.Println("RemainingBytes, hard code to 128")
+	return uint64(128)
+}
