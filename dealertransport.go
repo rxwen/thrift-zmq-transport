442feb75351149854cd51e9d8d4da6fb382b5723
Raymond Wen <rx.wen218@gmail.com>
2016-05-13 15:19:45 +0800

    add comments for DealerTransport

--- dealertransport.go
+++ dealertransport.go
@@ -12,6 +12,7 @@ type TDealerTransport struct {
 	endpoint string
 }
 
+// NewTDealerTransport function creates a new TDealerTransport instance.
 func NewTDealerTransport(endpoint string) TDealerTransport {
 	sock, _ := zmq.NewSocket(zmq.DEALER)
 
@@ -21,22 +22,26 @@ func NewTDealerTransport(endpoint string) TDealerTransport {
 	}
 }
 
+// Open method connects to remote endpoint.
 func (t TDealerTransport) Open() error {
 	fmt.Println("Open", t.endpoint)
 	return t.sock.Connect(t.endpoint)
 }
 
+// IsOpen method alwasy return true.
 func (t TDealerTransport) IsOpen() bool {
 	fmt.Println("IsOpen")
 	return true
 }
 
+// Close method shuts down the zmq socket.
 func (t TDealerTransport) Close() error {
 	fmt.Println("Close")
 	t.sock.SendBytes([]byte{}, 0) // send 0 bytes indicates close
 	return t.sock.Close()
 }
 
+// Read method get bytes from socket.
 func (t TDealerTransport) Read(buf []byte) (l int, err error) {
 	//fmt.Println("about to Read num of bytes ", len(buf))
 	data, err := t.sock.RecvBytes(0)
@@ -46,16 +51,19 @@ func (t TDealerTransport) Read(buf []byte) (l int, err error) {
 	return l, err
 }
 
+// Read method output bytes to socket.
 func (t TDealerTransport) Write(buf []byte) (int, error) {
 	//fmt.Println("Write ", buf)
 	return t.sock.SendBytes(buf, 0)
 }
 
+// Flush method performs noop.
 func (p TDealerTransport) Flush() error {
 	//fmt.Println("Flush")
 	return nil
 }
 
+// RemainingBytes method returns bytes available.
 func (p TDealerTransport) RemainingBytes() (num_bytes uint64) {
 	//fmt.Println("RemainingBytes, hard code to 128")
 	return uint64(4096)
